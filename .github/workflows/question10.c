eea3102fc3e81909bf32726de7673f24ccf747e3
Arunav Duggal <arunavduggal07@gmail.com>
2026-02-28 10:21:36 +0530

    edited code

--- .github/workflows/question10.c
+++ .github/workflows/question10.c
@@ -1,11 +1,23 @@
-def is_palindrome(s):
-    left = 0
-    right = len(s) - 1
-    while left < right:
-        if s[left] != s[right]:
-            return "NO"
-        left += 1
-        right -= 1
-    return "YES"
-s = input().strip()
-print(is_palindrome(s))
+#include <stdio.h>
+#include <string.h>
+
+const char* is_palindrome(char *s) {
+    int left = 0;
+    int right = strlen(s) - 1;
+    while (left < right) {
+        if (s[left] != s[right]) {
+            return "NO";
+        }
+        left++;
+        right--;
+    }
+    return "YES";
+}
+
+int main() {
+    char s[100];
+    if (scanf("%99s", s) == 1) {
+        printf("%s\n", is_palindrome(s));
+    }
+    return 0;
+}
