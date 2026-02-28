a1876dea17341630f8a491827532dda23e3e7f43
Arunav Duggal <arunavduggal07@gmail.com>
2026-02-28 10:21:02 +0530

    edited code

--- .github/workflows/question9.c
+++ .github/workflows/question9.c
@@ -1,6 +1,21 @@
-def mirror_name(s):
-    if len(s) <= 1:
-        return s
-    return s[-1] + mirror_name(s[:-1])
-code_name = input().strip()
-print(mirror_name(code_name))
+#include <stdio.h>
+#include <string.h>
+
+void mirror_name(char *s, int start, int end) {
+    if (start >= end) {
+        return;
+    }
+    char temp = s[start];
+    s[start] = s[end];
+    s[end] = temp;
+    mirror_name(s, start + 1, end - 1);
+}
+
+int main() {
+    char code_name[100];
+    if (scanf("%99s", code_name) == 1) {
+        mirror_name(code_name, 0, strlen(code_name) - 1);
+        printf("%s\n", code_name);
+    }
+    return 0;
+}
