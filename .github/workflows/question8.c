21c0d5e0b84e6c3ec3736bc24458c7aee6fb6ce3
Arunav Duggal <arunavduggal07@gmail.com>
2026-02-28 10:20:23 +0530

    edited code

--- .github/workflows/question8.c
+++ .github/workflows/question8.c
@@ -1,9 +1,17 @@
-def power(a, b):
-    if b == 0:
-        return 1
-    else:
-        return a * power(a, b - 1)
-user_input = input().split()
-base = int(user_input[0])
-exp = int(user_input[1])
-print(power(base, exp))
+#include <stdio.h>
+
+long long power(int a, int b) {
+    if (b == 0) {
+        return 1;
+    } else {
+        return a * power(a, b - 1);
+    }
+}
+
+int main() {
+    int base, exp;
+    if (scanf("%d %d", &base, &exp) == 2) {
+        printf("%lld\n", power(base, exp));
+    }
+    return 0;
+}
