d523d9251183901f6059d4f468a6ec45312e2e42
Arunav Duggal <arunavduggal07@gmail.com>
2026-04-02 18:10:52 +0530

    solved and tested with sample

---
+++ .github/workflows/question13.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+void printSpiral(int r, int c) 
+{
+    int matrix[r][c];
+    for (int i=0;i<r;i++) 
+    {
+        for (int j=0;j<c;j++) 
+        {
+            scanf("%d" &matrix[i][j]);
+        }
+    }
+    int top=0,bottom=r-1;
+    int left=0,right=c-1;
+    int first=1;
+    while (top<=bottom && left<=right) 
+    {
+        for (int i=left;i<=right;i++) 
+        {
+            if (!first) printf(" ");
+            printf("%d", matrix[top][i]);
+            first=0;
+        }
+        top++;
+        for (int i=top;i<=bottom;i++) 
+        {
+            printf(" %d", matrix[i][right]);
+        }
+        right--;
+        if (top<=bottom) 
+        {
+            for (int i=right;i>=left;i--) 
+            {
+                printf(" %d",matrix[bottom][i]);
+            }
+            bottom--;
+        }
+        if (left<=right) 
+        {
+            for (int i=bottom;i>=top;i--) 
+            {
+                printf(" %d", matrix[i][left]);
+            }
+            left++;
+        }
+    }
+    printf("\n");
+}
+int main() 
+{
+    int r,c;
+    if (scanf("%d %d", &r, &c)==2) 
+    {
+        printSpiral(r,c);
+    }
+    return 0;
+}
