7caf91460e96adda2a3b7e07c20e73fe2c882fa6
Arunav Duggal <arunavduggal07@gmail.com>
2026-05-02 10:09:50 +0530

    edited code

--- .github/workflows/question11.c
+++ .github/workflows/question11.c
@@ -1,14 +1,38 @@
-
-m, n = map(int, input().split())
-matrix_a = []
-for _ in range(m):
-    matrix_a.append(list(map(int, input().split())))
-matrix_b = []
-for _ in range(m):
-    matrix_b.append(list(map(int, input().split())))
-for i in range(m):
-    row_sum = []
-    for j in range(n):
-        element_sum = matrix_a[i][j] + matrix_b[i][j]
-        row_sum.append(str(element_sum))
-    print(" ".join(row_sum))
+#include <stdio.h>
+int main() {
+    int rows, cols, i, j;
+    printf("Enter number of rows and columns: ");
+    scanf("%d %d", &rows, &cols);
+    int A[rows][cols], B[rows][cols], sum[rows][cols];
+    printf("\nEnter elements of Matrix A:\n");
+    for (i = 0; i < rows; i++) {
+        for (j = 0; j < cols; j++) {
+            scanf("%d", &A[i][j]);
+        }
+    }
+    printf("\nEnter elements of Matrix B:\n");
+    for (i = 0; i < rows; i++) 
+    {
+        for (j = 0; j < cols; j++) 
+        {
+            scanf("%d", &B[i][j]);
+        }
+    }
+    for (i = 0; i < rows; i++) 
+    {
+        for (j = 0; j < cols; j++) 
+        {
+            sum[i][j] = A[i][j] + B[i][j];
+        }
+    }
+    printf("\nSum of the matrices:\n");
+    for (i = 0; i < rows; i++) 
+    {
+        for (j = 0; j < cols; j++) 
+        {
+            printf("%d\t", sum[i][j]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
